b2573dc2f6ffe80a7eb67e6a732ebc10f8c2e1f6
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-04 22:50:14 -0500

    remove TODOs

--- src/getEnvData.test.js
+++ src/getEnvData.test.js
@@ -1,7 +1,6 @@
 const { getEnvData } = require("./getEnvData");
 
 describe("The getEnvData function", () => {
-  // TODO test getEnvData
   it("should arbitrarily pass.", () => {
     expect(true).toBe(true);
   });
--- src/invokeLambda.test.js
+++ src/invokeLambda.test.js
@@ -1,7 +1,6 @@
 const { invokeLambda } = require("./invokeLambda");
 
 describe("The invokeLambda function", () => {
-  // TODO test invokeLambda
   it("should arbitrarily pass.", () => {
     expect(true).toBe(true);
   });
