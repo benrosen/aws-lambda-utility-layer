3595fdebda8a40941dfdf6094f3c4f9cb8b817d4
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-04 21:25:58 -0500

    create and configure test files

---
+++ src/getEnvData.test.js
@@ -0,0 +1,8 @@
+const { getEnvData } = require("./getEnvData");
+
+describe("The getEnvData function", () => {
+  // TODO test getEnvData
+  it("should fail.", () => {
+    expect(true).toBe(false);
+  });
+});
---
+++ src/invokeLambda.test.js
@@ -0,0 +1,8 @@
+const { invokeLambda } = require("./invokeLambda");
+
+describe("The invokeLambda function", () => {
+  // TODO test invokeLambda
+  it("should fail.", () => {
+    expect(true).toBe(false);
+  });
+});
