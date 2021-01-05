8fcce453f62de16282dceb9ee66505f665f9939e
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-04 21:16:06 -0500

    implement basic functions

--- src/getEnvData.js
+++ src/getEnvData.js
@@ -1,6 +1,17 @@
 /**
  * Get data from the Lambda environment.
+ *
+ * @module getEnvData
+ * @param {*} environment The Lambda environment.
+ * @param {string} key The key of the environment data you want to get.
+ * @returns {*} The environment data.
  */
-module.exports = function getEnvData() {
-  // TODO implement getEnvData
+module.exports = function getEnvData(environment, key) {
+  const data = environment[key];
+  if (data) {
+    return data;
+  }
+  // TODO extract to custom error class
+  // TODO document @throws custom error type
+  throw new Error(`Function requires environment variable: '${key}'`);
 };
--- src/invokeLambda.js
+++ src/invokeLambda.js
@@ -1,6 +1,18 @@
 /**
- * Invoke a Lambda function and return the results.
+ * Invoke a Lambda function and return the result.
+ *
+ * @module invokeLambda
+ * @param {*} client The Lambda client to use.
+ * @param {string} functionName The name of the Lambda function to invoke.
+ * @param {*} payload The Lambda function payload.
+ * @returns {*} The payload returned from the Lambda function.
  */
-module.exports = function invokeLambda() {
-  // TODO: implement invokeLambda
+module.exports = function invokeLambda(client, functionName, payload) {
+  return client
+    .invoke({
+      FunctionName: functionName,
+      Payload: JSON.stringify(payload),
+    })
+    .promise()
+    .then((response) => JSON.parse(response.Payload));
 };
