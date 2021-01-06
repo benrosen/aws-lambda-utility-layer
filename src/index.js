c1e8e39ea28be526e55c6561822b74b284276978
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-06 01:31:20 -0500

    export createRecord

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const createRecord = require("./createRecord.js");
 const getEnvData = require("./getEnvData.js");
 const invokeLambda = require("./invokeLambda.js");
 
 module.exports = {
+  createRecord,
   getEnvData,
   invokeLambda,
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,13 @@
 const { expect } = require("@jest/globals");
-const { getEnvData, invokeLambda } = require("./index.js");
+const { createRecord, getEnvData, invokeLambda } = require("./index.js");
 
 describe("The aws-lambda-utility-layer package", () => {
+  describe("createRecord function", () => {
+    it("should be defined.", () => {
+      expect(createRecord).toBeDefined();
+    });
+  });
+
   describe("getEnvData function", () => {
     it("should be defined.", () => {
       expect(getEnvData).toBeDefined();
