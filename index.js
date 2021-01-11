6e187c668a519f7c01860688ecbe2d99e706300e
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-11 15:49:02 -0500

    move index.js to root

---
+++ index.js
@@ -0,0 +1,11 @@
+const createRecord = require("./src/createRecord");
+const getEnvData = require("./src/getEnvData");
+const invokeLambda = require("./src/invokeLambda");
+const updateRecord = require("./src/updateRecord");
+
+module.exports = {
+  createRecord,
+  getEnvData,
+  invokeLambda,
+  updateRecord,
+};
