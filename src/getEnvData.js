5b6bc0d7baa26f437d36e0a08adb9ce4017afe75
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-18 09:45:23 -0500

    accept multiple keys

--- src/getEnvData.js
+++ src/getEnvData.js
@@ -3,13 +3,29 @@
  *
  * @module getEnvData
  * @param {*} environment The Lambda environment.
- * @param {string} key The key of the environment data you want to get.
- * @returns {*} The environment data.
+ * @param {string[]} keys The key of the environment data you want to get.
+ * @returns {*[]} A list of environment data values.
  */
-module.exports = function getEnvData(environment, key) {
-  const data = environment[key];
-  if (data) {
-    return data;
+module.exports = function getEnvData(environment, keys) {
+  if (!environment) {
+    throw new TypeError("Missing required argument: environment.");
   }
-  throw new Error(`Function requires environment variable: '${key}'`);
+
+  if (!keys) {
+    throw new TypeError("Missing required argument: keys.");
+  }
+
+  const getData = (key) => {
+    const data = environment[key];
+    if (data) {
+      return data;
+    }
+    throw new Error(`Missing required environment variable: '${key}'`);
+  };
+
+  if (keys.length === 1) {
+    return getData(keys[0]);
+  }
+
+  return keys.map((key) => getData(key));
 };
--- src/getEnvData.test.js
+++ src/getEnvData.test.js
@@ -1,7 +1,23 @@
 const getEnvData = require("./getEnvData");
 
 describe("The getEnvData function", () => {
-  it("should arbitrarily pass.", () => {
+  it("should be defined.", () => {
     expect(getEnvData).toBeDefined();
+  });
+
+  it("should throw a MissingArgumentError if the environment argument is missing.", () => {
+    //
+  });
+
+  it("should throw a MissingArgumentError if the keys argument is missing.", () => {
+    //
+  });
+
+  it("should throw a MissingEnvironmentVariableError if a key is provided for an undefined environment variable.", () => {
+    //
+  });
+
+  it("should return an array containing a value for each key in the keys argument.", () => {
+    //
   });
 });
